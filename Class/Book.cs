d82a3da960a837ae2a4efa1183d168fb3cd46a7d
DeveloperAbdumalik <dreamyoutobe1@gmail.com>
2024-10-30 19:51:43 +0500

    first commit

---
+++ Class/Book.cs
@@ -0,0 +1,52 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace BOOK.Class
+    {
+        public class Book
+        {
+            private string title;
+            private string author;
+            private int price;
+
+
+            public string Title { get; set; }
+            public string Author { get; set; } 
+            public int Price { get; set; }
+
+            public int deferance {get; set;}
+
+
+            public Book(string name,int narx)
+            {
+                Title = name;
+                price = narx;
+
+            }
+
+              public void DisplayInfo()  
+            {
+                Console.WriteLine($"Kitob: {Title}, Muallif: {Author}, Narxi: {Price:C}");
+            }
+            
+            public void DefirencePriceCalculate(int price)
+            {
+                if(Price>price) 
+                {
+                    deferance = Price-price;
+                    Console.WriteLine($"Siz sotib olmoqchi bulgan kitob {deferance } ga Arzon siz ola olasiz:");
+                }
+                else if (Price<price) 
+                {
+                     deferance = price-Price;
+                     Console.WriteLine($"Siz sotib olmoqchi bulgan kitob {deferance } ga qimmat:");
+                }
+                else deferance = (0); Console.WriteLine($"Siz sotib olmoqchi bulgan kitobni ola olasiz:");
+            }
+
+            public void DefirencePrice()
+            {
+                Console.WriteLine($"Siz sotib olmoqchi bulgan kitob {deferance } ga qimmat:");
+            }
+
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using BOOK.Class;
+Console.WriteLine("Kitob nomini kiriting");
+string name = Console.ReadLine();
+Console.WriteLine("Qanchaga sotib olmoqchisiz");
+int narx = Convert.ToInt32(Console.ReadLine());
+Book book = new Book(name, narx);
+
+book.Author = "Malikshox";
+book.Price = 236;
+book.DisplayInfo();
+book.DefirencePriceCalculate(narx);
+book.DefirencePrice();
